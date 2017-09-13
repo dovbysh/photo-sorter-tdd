2f9284ba06f7cda3e8d71ab073130071be948525
Andrey Dovbyshko <dovbysh@gmail.com>
2017-09-13 16:17:37 +0300

    Rename constants

--- src/Message.php
+++ src/Message.php
@@ -7,9 +7,9 @@ class Message
 {
     const TYPE_SKIPPED = 1;
     const TYPE_UNABLE_TO_DETERMIN_FILE_DATE = 2;
-    const TYPE_fileExistsAndHasDifferentSize = 3;
-    const TYPE_successCopied = 4;
-    const TYPE_failedToCopy = 5;
+    const TYPE_FILE_EXISTS_AND_HAS_DIFFERENT_SIZE = 3;
+    const TYPE_SUCCESS_COPIED = 4;
+    const TYPE_FAILED_TO_COPY = 5;
 
     public function skipped(string $fileName)
     {
@@ -26,13 +26,13 @@ class Message
             case self::TYPE_UNABLE_TO_DETERMIN_FILE_DATE:
                 $prefix = '[UnableToDetermineFileDate] ';
                 break;
-            case self::TYPE_fileExistsAndHasDifferentSize:
+            case self::TYPE_FILE_EXISTS_AND_HAS_DIFFERENT_SIZE:
                 $prefix = '[FileExistsAndHasDifferentSize] ';
                 break;
-            case self::TYPE_successCopied:
+            case self::TYPE_SUCCESS_COPIED:
                 $prefix = '[OK] ';
                 break;
-            case self::TYPE_failedToCopy:
+            case self::TYPE_FAILED_TO_COPY:
                 $prefix = '[FailedToCopy] ';
                 break;
         }
@@ -48,18 +48,18 @@ class Message
     public function fileExistsAndHasDifferentSize(string $sourceFile, string $destinationFile, int $sourceSize, int $destinationSize)
     {
         $message = $sourceFile . ' - ' . $sourceSize . '; ' . $destinationFile . ' - ' . $destinationSize;
-        return $this->log($message, self::TYPE_fileExistsAndHasDifferentSize);
+        return $this->log($message, self::TYPE_FILE_EXISTS_AND_HAS_DIFFERENT_SIZE);
     }
 
     public function successCopied(string $sourceFile, string $destinationFile)
     {
         $message = $sourceFile . ' -> ' . $destinationFile;
-        return $this->log($message, self::TYPE_successCopied);
+        return $this->log($message, self::TYPE_SUCCESS_COPIED);
     }
 
     public function failedToCopy(string $sourceFile, string $destinationFile)
     {
         $message = $sourceFile . ' -> ' . $destinationFile;
-        return $this->log($message, self::TYPE_failedToCopy);
+        return $this->log($message, self::TYPE_FAILED_TO_COPY);
     }
 }
